97e7c8b21cba493d170f8be8a33b7275b97f4054
dkmalakar <111627819+dkmalakar@users.noreply.github.com>
2022-08-20 12:57:38 +0530

    Create hello world.java
    new project add

---
+++ hello world.java
@@ -0,0 +1,6 @@
+public class helloword {
+ 
+          public static void main (string[] arge {
+               // print "hello word " to the terminal window.
+               system.out.printin("hello,world);
+}
